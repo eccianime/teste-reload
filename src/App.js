28c53b2cfe4691e8fa3f2520d7dca870bc9126ad
ingjeanpaulrojas <ingjeanpaulrojas@gmail.com>
2020-08-19 14:20:59 -0300

    Added SVG as a component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Avatar from './components/Avatar';
 
 function App() {
 	return (
-		<div>
-			
-		</div>
+		<Container><Row className="justify-content-center text-center">
+			<Col sm={12} md={6} className="simple-card">
+				<h1>Avatar Test</h1>
+			</Col>
+			<Col sm={12} md={6} className="simple-card">
+				<Avatar />
+			</Col>
+		</Row></Container>
 	);
 }
 
